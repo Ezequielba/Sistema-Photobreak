ceb315157c02725c11794bc58b5b38c80adb52a7
Ezequiel <3zequiel@gmail.com>
2020-06-27 13:58:03 -0300

    Projeto criado

---
+++ src/main/java/br/com/photobreak/SisPhotobreak/SisPhotobreakApplication.java
@@ -0,0 +1,13 @@
+package br.com.photobreak.SisPhotobreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisPhotobreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisPhotobreakApplication.class, args);
+	}
+
+}
